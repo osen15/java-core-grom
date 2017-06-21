7d55a5c8dee2807da47b32587cf519dcab111760
Oleg <senyuholeg@gmail.com>
2017-06-21 12:51:49 +0300

    singelton update

--- src/lesson14/Demo.java
+++ src/lesson14/Demo.java
@@ -4,24 +4,19 @@ package lesson14;
 public class Demo {
     public static void main(String[] args) {
 
-
         Object objectTest = new Object();
 
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
 
-
         objectTest = new Object();
 
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
 
-
         objectTest = new Object();
 
         System.out.println(objectTest.toString());
         System.out.println(objectTest.getClass().getName());
-
-
     }
 }
