8c4d5224b14e1223c0bda3acfd04a8b264656c43
Oleg <senyuholeg@gmail.com>
2017-06-25 20:39:39 +0200

    update

--- src/lesson16/StringCompare.java
+++ src/lesson16/StringCompare.java
@@ -5,27 +5,29 @@ import java.util.Arrays;
 
 public class StringCompare {
     public static void main(String[] args) {
-        System.out.println("abc" == "abc");
         System.out.println(new String("abc") == new String("abc"));
         System.out.println(new String("abc").equals(new String("abc")));
-        System.out.println("abc".equals("abc"));
-
+        System.out.println("abc" == "abc");
+        System.out.println("Abc".equals("abc"));
 
         String s1 = "test";
         String s2 = "test";
 
-        // using intern
-        String s3 = new String("pppp");
+        System.out.println(s1 == s2);
+        //using intern
+        String s3 = new String ("pppp");
         String s4 = "pppp";
         System.out.println(s3 == s4);
-        System.out.println(s3.intern() == s4);   //заганяє примусово об єкт в String pool
+        s3 = s3.intern();
+        System.out.println(s3 == s4);
 
-
-        // byte of String
+        //bytes of string
         String str = "testStringVar";
-        // Кожний символ має своє число, яке береться в ASCII Table
-        System.out.println(Arrays.toString(str.getBytes()));   // перетворює String в байти для передачі з точки А в точку Б.
-        System.out.println(new String(str.getBytes()));
+        System.out.println(Arrays.toString(str.getBytes()));
+
+        System.out.println(new String(str.getBytes()) );
+
+        // test.getBytes();
 
 
 
