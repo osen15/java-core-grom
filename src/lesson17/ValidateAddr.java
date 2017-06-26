32f83d954d261098009966d16b88628972054056
Oleg <senyuholeg@gmail.com>
2017-06-26 22:46:30 +0200

    update

---
+++ src/lesson17/ValidateAddr.java
@@ -0,0 +1,23 @@
+package lesson17;
+
+public class ValidateAddr {
+    public static void main(String[] args) {
+        String test = "http://gromcode.com";
+        System.out.println(validate(test));
+
+    }
+
+    private static boolean validate(String address) {
+
+        String[] domens = new String[]{".com", ".org", ".net"};
+        address.trim();
+        if (address.substring(0, 7).equals("http://") || (address.substring(0, 8).equals("https://"))) {
+            for (String domen : domens) {
+                if (address.substring(address.length() - 4, address.length()).equals(domen)) {
+                    return true;
+                } else return false;
+            }
+        }
+        return false;
+    }
+}
