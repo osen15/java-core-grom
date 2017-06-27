5a1ac7efdabf64be80a32cfbfb457be59665bebd
Oleg <senyuholeg@gmail.com>
2017-06-27 16:08:27 +0200

    update regex

--- src/lesson17/MinAndMaxWords.java
+++ src/lesson17/MinAndMaxWords.java
@@ -2,7 +2,7 @@ package lesson17;
 
 public class MinAndMaxWords {
     public static void main(String[] args) {
-        String test = "qw we wewewe wewewe l wewewewewedfsdf";
+        String test = "qw we wewewe wewewe l";
         System.out.println(maxWord(test));
         System.out.println(minWord(test));
 
@@ -10,10 +10,12 @@ public class MinAndMaxWords {
 
 
     private static String maxWord(String input) {
-        String[] words = input.split(" ");
+        if (input.isEmpty()) return null;
+        String string =input.replaceAll("\\pP", "");
+        String[] words = string.split(" ");
         String max = words[0];
         for (String word : words) {
-            if (word.length() > max.length()) {
+            if (word.length() > max.length() && word.matches("^[а-яА-ЯёЁa-zA-Z]+$")) {
                 max = word;
             }
         }
@@ -21,7 +23,9 @@ public class MinAndMaxWords {
     }
 
     private static String minWord(String input) {
-        String[] words = input.split(" ");
+        if (input.isEmpty()) return null;
+        String string =input.replaceAll("\\pP", "");
+        String[] words = string.split(" ");
         String min = words[0];
         for (String word : words) {
             if (word.length() < min.length()) {
