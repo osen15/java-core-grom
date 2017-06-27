84cfba0ca29adc6969474ffb1d3d7064012b1301
Oleg <senyuholeg@gmail.com>
2017-06-27 12:50:36 +0200

    update regex

--- src/lesson17/CountW.java
+++ src/lesson17/CountW.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 
 public class CountW {
     public static void main(String[] args) {
-        String res = "! % @ ^ ї S m";
+        String res = "! G G G g ] g l c 3 & *";
         System.out.println(countWords(res));
 
     }
 
     private static int countWords(String input) {
-        if(input == null)
-            return 0;
+        if(input == null && input.toCharArray().length == 0)  return 0;
         int count = 0;
         String[] words = input.split(" ");
         for (String word : words) {
