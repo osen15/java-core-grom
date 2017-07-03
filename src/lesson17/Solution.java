fe5cf5f740a98dd0e983f42dcee750f3a089434d
Oleg <senyuholeg@gmail.com>
2017-07-03 21:57:39 +0200

    update regex

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -4,42 +4,39 @@ package lesson17;
 
 public class Solution {
     public static void main(String[] args) {
-        String test = "sq'wwet1 1111 gfg er1 vbnmm  gfgf";
+        String test = "sq'wwet2 1111 gfg er1 vbnmm  gfgf";
         System.out.println(maxWord((test)));
         System.out.println(minWord(test));
     }
     private static String maxWord(String input) {
-        if (input.trim().isEmpty() || input == null) return null;  // обрізаємо рядок і перевіряємо чи порожній і на null
-        String[] words = (input + " ").trim().split("\\p{P}?[ \\t\\n\\r]+"); // виключаємо всі розділові знаки і пробіли
-        if (words.length == 0) return null;  // перевірка довжини масива
-        String max = checkWords(words)[0];
+        if (input == null) return null;  // на null
+        String[] words =input.trim().split(" ");
+        String max = words[0];
         for (String word : words) {
-            if (word.length() > max.length())
+            if (checkWord(word) && word.length() > max.length())
                 max = word;
         }
         return max;
     }
     private static String minWord(String input) {
-        if (input.trim().isEmpty() || input == null) return null;
-        String[] words = (input + " ").trim().split("\\p{P}?[ \\t\\n\\r]+");
-        if (words.length == 0) return null;
-        String min = checkWords(words)[0];
+        if (input == null) return null;
+        String[] words = input.trim().split(" ");
+        String min = (words)[0];
 
         for (String word : words) {
-            if (word.length() < min.length())
+            if (!word.equals("") && checkWord(word) && word.length() < min.length())
                 min = word;
         }
         return min;
     }
 
-    private static String[] checkWords(String[] words) {   // перевірка символів на букву
-        for (int i = 0; i < words.length; i++) {
-            for (char letter : words[i].toCharArray()) {
-                if (!Character.isLetter(letter))
-                    words[i] = String.valueOf("".isEmpty());
-            }
-        }
-        return words;
+    private static boolean checkWord(String word) {   // перевірка символів на букву
+       char[] chars = word.toCharArray();
+       for (char ch : chars){
+           if (!Character.isLetter(ch))
+               return  false;
+       }
+       return  true;
 
     }
 }
