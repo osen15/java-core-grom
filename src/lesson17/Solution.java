5793501e5bcba7e162e7311c08d83cdacc0d00e2
Oleg <senyuholeg@gmail.com>
2017-07-05 16:18:49 +0200

    minmax udate

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -3,49 +3,44 @@ package lesson17;
 
 public class Solution {
     public static void main(String[] args) {
-        String test = "1";
+        String test = "1 re    ";
         System.out.println(maxWord((test)));
         System.out.println(minWord(test));
     }
 
     private static String maxWord(String input) {
-        if (input.isEmpty() || input == null) return null;  // на null
-        String[] words = input.trim().split(" ");
-        if (words.length == 0) return null;
-        String max = words[0];
+        if (input.isEmpty() || input == null) return null; // перевірка на порожній рядок і null
+        String[] words = input.trim().split(" "); // обрізаємо з країв і розбиваємо на слова
+        String max = checkWordZero(words, words[0]);
         for (String word : words) {
-            if (checkWord(word)) {
+            if (checkWord(word) && !word.equals("")) {
                 if (word.length() > 0 && word.length() > max.length()) {
                     max = word;
-                    return max;
                 }
-                if (words.length > 0 && words.length == 1 ) {
+                if (words.length > 0 && words.length == 1) {
                     max = word;
-                    return max;
                 }
+
             }
         }
-        return "ERROR";
+        return max;
     }
 
     private static String minWord(String input) {
         if (input.equals("") || input == null) return null;
         String[] words = input.trim().split(" ");
-        if (words.length == 0) return null;
-        String min = (words)[0];
+        String min = checkWordZero(words, words[0]);
         for (String word : words) {
-            if (checkWord(word)) {
+            if (checkWord(word) && !word.equals("")) {
                 if (word.length() > 0 && word.length() < min.length()) {
                     min = word;
-                    return min;
                 }
-                if (word.length() > 0 && words.length == 1){
+                if (word.length() > 0 && words.length == 1) {
                     min = word;
-                    return min;
                 }
             }
         }
-        return "ERROR";
+        return min;
     }
 
     private static boolean checkWord(String word) {   // перевірка символів на букву
@@ -55,5 +50,17 @@ public class Solution {
                 return false;
         }
         return true;
+    }
+
+    private static String checkWordZero(String[] words, String word1) { // метод заміни числа на слово, якщо є таке.
+        for (String word : words) {
+            if (checkWord(word) && !checkWord(word1) && !word.equals("") && !word1.equals("")) {
+                word1 = word;
+                return word1;
+            }
+            if (checkWord(word) && checkWord(word1) && !word.equals("") && !word1.equals(""))
+                return word1;
+        }
+        return null;
     }
 }
