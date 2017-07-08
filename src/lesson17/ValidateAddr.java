9e9f7360cac1abf1c94e54513a54d1b2ae7f1408
Oleg <senyuholeg@gmail.com>
2017-07-08 20:27:49 +0200

    validateaddress update

--- src/lesson17/ValidateAddr.java
+++ src/lesson17/ValidateAddr.java
@@ -3,50 +3,64 @@ package lesson17;
 
 public class ValidateAddr {
     public static void main(String[] args) {
-        String test = "http://1.com";
+        String test = "http://dk1.net";
         System.out.println(validate(test));
 
     }
 
     private static boolean validate(String address) {
-        if (address == null)  // перевірка адреси на null
+        if (address == null || address.length() <= 2) // перевірка на нул і <=2
             return false;
-        String[] domens = new String[]{".com", ".org", ".net"};  // створюю масив з назвами доменів
-        if (address.length() >= 12 // мінімально можлива довжина адреса
-                && checkAddressOnWhiteSpace(address) // перевірка на пробіли
-                && address.substring(0, 7).equals("http://")  // прирівнюю до протоколу
-                && checkWord(address.substring(8, address.length() - 4)) // перевіряю назву адреси
+        String[] domens = new String[]{".com", ".org", ".net"};  // масив з доменів
+        if (checkAddressOnWhiteSpace(address))      // перевірка на пробіли
+            return false;
+        if (!checkName(address))    // перевірка імені
+            return false;
+        if (!checkDomen(address, domens))   // перевірка домена
+            return false;
 
-                || address.length() >= 13   // аналогічно з другим протоколом https://
-                && checkAddressOnWhiteSpace(address)
-                && (address.substring(0, 8).equals("https://"))
-                && checkWord(address.substring(9, address.length() - 4))) {
-                                                                                 // якщо тру, то перевіряємо домен
-            for (String domen : domens) {
-                if (address.substring(address.length() - 4, address.length()).equals(domen)) { // прирівнюємо домен адреси до доступних доменів
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    private static boolean checkWord(String word) {   // перевірка символів на букву і цифру
-        char[] chars = word.toCharArray();
-        for (char ch : chars) {
-            if (!Character.isLetterOrDigit(ch))
-                return false;
-        }
         return true;
     }
 
-    private static boolean checkAddressOnWhiteSpace(String address) {   //метод перевіряє на вайтспейс і чи масив з стрінгів менший за 2
+    private static boolean checkName(String address) {   // перевірка ім"я адреси 1 і 2 домена
+        if (address.substring(0, 7).equals("http://")) {
+            if (address.length() <= 11) return false;
+            char[] chars = address.substring(7, address.length() - 4).toCharArray();
+            for (char ch : chars) {
+                if (!Character.isLetterOrDigit(ch))
+                    return false;
+
+            }
+        }
+        if (address.substring(0, 8).equals("https://") && address.length() >= 12) {
+            if (address.length() <= 12) return false;
+            char[] chars = address.substring(8, address.length() - 4).toCharArray();
+            for (char ch : chars) {
+                if (!Character.isLetterOrDigit(ch))
+                    return false;
+
+            }
+        }
+        return true;
+    }
+    private static boolean checkAddressOnWhiteSpace(String address) {  // перевірка на пробіл
         String[] simbols = address.split("");
         if (simbols.length > 2) {
             for (String simb : simbols) {
                 if (Character.isWhitespace(simb.charAt(0)))
-                    return false;
+                    return true;
             }
         }
-        return true;
+        return false;
     }
+
+    private static boolean checkDomen(String address, String[] domens) {    // перевірка домена
+        for (String domen : domens) {
+            if (address.substring(address.length() - 4, address.length()).equals(domen)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
