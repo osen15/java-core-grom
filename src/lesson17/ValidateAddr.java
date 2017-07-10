43736685be6d68fe5cae90ded5851145a1667c9b
Oleg <senyuholeg@gmail.com>
2017-07-10 14:25:27 +0200

    validateaddress update

--- src/lesson17/ValidateAddr.java
+++ src/lesson17/ValidateAddr.java
@@ -3,64 +3,35 @@ package lesson17;
 
 public class ValidateAddr {
     public static void main(String[] args) {
-        String test = "http://dk1.net";
+        String test = "http://www.  .com";
         System.out.println(validate(test));
 
     }
-
     private static boolean validate(String address) {
-        if (address == null || address.length() <= 2) // перевірка на нул і <=2
+        if (!address.startsWith("http://") || !address.startsWith("https://"))
             return false;
-        String[] domens = new String[]{".com", ".org", ".net"};  // масив з доменів
-        if (checkAddressOnWhiteSpace(address))      // перевірка на пробіли
-            return false;
-        if (!checkName(address))    // перевірка імені
-            return false;
-        if (!checkDomen(address, domens))   // перевірка домена
-            return false;
+        address = address.replace("http://", "");
+        address = address.replace("https://", "");
 
-        return true;
+        address = address.replace("www.", "");
+
+
+        if (!address.contains("net.") || !address.contains("com.") || !address.contains("org."))
+            return false;
+        address = address.replace("net.", "");
+        address = address.replace("com.", "");
+        address = address.replace("org.", "");
+
+        return  checkWord(address);
+
     }
-
-    private static boolean checkName(String address) {   // перевірка ім"я адреси 1 і 2 домена
-        if (address.substring(0, 7).equals("http://")) {
-            if (address.length() <= 11) return false;
-            char[] chars = address.substring(7, address.length() - 4).toCharArray();
-            for (char ch : chars) {
-                if (!Character.isLetterOrDigit(ch))
-                    return false;
-
-            }
-        }
-        if (address.substring(0, 8).equals("https://") && address.length() >= 12) {
-            if (address.length() <= 12) return false;
-            char[] chars = address.substring(8, address.length() - 4).toCharArray();
-            for (char ch : chars) {
-                if (!Character.isLetterOrDigit(ch))
-                    return false;
-
-            }
+    private static boolean checkWord(String word) {   // перевірка символів на букву
+        char[] chars = word.toCharArray();
+        for (char ch : chars) {
+            if (!Character.isLetterOrDigit(ch))
+                return false;
         }
         return true;
-    }
-    private static boolean checkAddressOnWhiteSpace(String address) {  // перевірка на пробіл
-        String[] simbols = address.split("");
-        if (simbols.length > 2) {
-            for (String simb : simbols) {
-                if (Character.isWhitespace(simb.charAt(0)))
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean checkDomen(String address, String[] domens) {    // перевірка домена
-        for (String domen : domens) {
-            if (address.substring(address.length() - 4, address.length()).equals(domen)) {
-                return true;
-            }
-        }
-        return false;
     }
 
 }
