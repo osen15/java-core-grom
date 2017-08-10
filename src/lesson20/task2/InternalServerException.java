1e698d76e12cec5a0156eedd44e65a12b2f88a96
Oleg <senyuholeg@gmail.com>
2017-08-10 14:47:08 +0200

    fix

--- src/lesson20/task2/InternalServerException.java
+++ src/lesson20/task2/InternalServerException.java
@@ -1,6 +1,6 @@
 package lesson20.task2;
 
-public class InternalServerException extends  Exception {
+public class InternalServerException extends BadRequestException {
     public InternalServerException(String message) {
         super(message);
     }
