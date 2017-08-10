b76a04b5f86cc51093b988ede210dde0cc870fe5
Oleg <senyuholeg@gmail.com>
2017-08-10 14:49:22 +0200

    fix

--- src/lesson20/task2/LimitExceeded.java
+++ src/lesson20/task2/LimitExceeded.java
@@ -1,6 +1,6 @@
 package lesson20.task2;
 
-public class LimitExceeded extends Exception {
+public class LimitExceeded extends BadRequestException {
     public LimitExceeded(String message) {
         super(message);
     }
