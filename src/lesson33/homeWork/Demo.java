08791b2d3ff849dea05228c7e4d725e0df4385ce
Oleg <senyuholeg@gmail.com>
2017-11-14 20:38:15 +0100

    first commit

--- src/lesson33/homeWork/Demo.java
+++ src/lesson33/homeWork/Demo.java
@@ -6,13 +6,13 @@ package lesson33.homeWork;
 public class Demo {
     public static void main(String[] args) throws Exception {
 
-     //   WriteToFileFromConsole writeToFileFromConsole = new WriteToFileFromConsole();
+        WriteToFileFromConsole writeToFileFromConsole = new WriteToFileFromConsole();
 
-     //   writeToFileFromConsole.writeToFileFromConsole("C:/Test.txt");
+        writeToFileFromConsole.writeToFileFromConsole("C:/Test.txt");
 
-        ReadFileByConsolePath readFileByConsolePath = new ReadFileByConsolePath();
+      //  ReadFileByConsolePath readFileByConsolePath = new ReadFileByConsolePath();
 
-        readFileByConsolePath.readFileByConsolePath();
+      //  readFileByConsolePath.readFileByConsolePath();
 
     }
 
--- src/lesson33/homeWork/WriteToFileFromConsole.java
+++ src/lesson33/homeWork/WriteToFileFromConsole.java
@@ -3,28 +3,13 @@ package lesson33.homeWork;
 import org.apache.commons.io.IOUtils;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class WriteToFileFromConsole {
     public void writeToFileFromConsole(String path) throws Exception {
+        WriteToFileFromConsole writeToFileFromConsole = new WriteToFileFromConsole();
 
-        System.out.println("Enter file content to write in the file:");
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String text = bufferedReader.readLine();
-
-        File file;
-
-        file = new File("C:/Test.txt");
-        if (!file.canWrite()) {
-            bufferedReader.close();
-            throw new Exception("Can't write to file with path " + path);
-        }
-
-        if (!file.exists()) {
-            bufferedReader.close();
-            throw new Exception("File with path " + path + " not found");
-        }
+        writeToFileFromConsole.checkFile(path); // перевірка шляху і чи можна записати
 
         PrintWriter printWriter = null;
 
@@ -32,7 +17,12 @@ public class WriteToFileFromConsole {
 
             printWriter = new PrintWriter(new FileWriter(path, true));
 
-            printWriter.println(text);
+
+            for (String str : writeInConsole()) {  // виводить кожний введений рядок у файл з нового рядка
+                printWriter.println(str);
+
+
+            }
 
         } catch (IOException e) {
             System.err.println("Can't write to file");
@@ -40,7 +30,49 @@ public class WriteToFileFromConsole {
         } finally {
             IOUtils.closeQuietly(printWriter);
 
-            IOUtils.closeQuietly(bufferedReader);
         }
+    }
+
+
+
+    private void checkFile(String path) throws Exception {
+
+        File file;
+
+        file = new File("C:/Test.txt");
+        if (!file.canWrite()) {
+
+            throw new Exception("Can't write to file with path " + path);
+        }
+
+        if (!file.exists()) {
+
+            throw new Exception("File with path " + path + " not found");
+        }
+
+
+    }
+
+
+
+
+    private ArrayList<String> writeInConsole() throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+
+        System.out.println("Enter file content to write in the file:");
+
+        String text;
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+
+
+            while (!(text = bufferedReader.readLine()).equals("write")) {
+                list.add(text);
+            }
+
+        } catch (IOException e) {
+            System.err.println("error");
+        }
+        return list;
+
     }
 }
