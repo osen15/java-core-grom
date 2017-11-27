658bbd54ce863ad864494e435888c424a57ed1c7
Oleg <senyuholeg@gmail.com>
2017-11-27 11:49:27 +0100

    first commit

---
+++ src/lesson34/transferSentences/Demo.java
@@ -0,0 +1,8 @@
+package lesson34.transferSentences;
+
+public class Demo {
+    public static void main(String[] args) throws  Exception {
+        Solution solution = new Solution();
+        solution.transferSentences("C:/From.txt", "C:/To.txt", "1");
+    }
+}
