77666fff948aaf676deb964c2cf81a4745bce9de
Oleg <senyuholeg@gmail.com>
2017-12-13 10:36:18 +0100

    new commit

--- src/lesson35/controller/OrderController.java
+++ src/lesson35/controller/OrderController.java
@@ -1,17 +1,18 @@
 package lesson35.controller;
 
+import lesson35.service.OrderService;
+
 public class OrderController {
 
 
-    void bookRoom(long roomId, long userId, long hotelId){
-
+    public static void bookRoom(long roomId, long userId, long hotelId) throws Exception {
+        OrderService.bookRoom(roomId, userId, hotelId);
 
     }
 
 
-
-    void cancelReservation(long roomId, long userId, long hotelId){
-
+    public static void cancelReservation(long roomId, long userId) throws Exception {
+        OrderService.cancelReservation(roomId, userId);
 
     }
 }
