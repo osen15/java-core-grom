6ecc594dde157917728f2d6514bd52679a1fff9a
Oleg <senyuholeg@gmail.com>
2017-12-19 20:10:54 +0100

    new commit

--- src/lesson35/DAO/utils/ReadFromFile.java
+++ src/lesson35/DAO/utils/ReadFromFile.java
@@ -1,16 +1,30 @@
 package lesson35.DAO.utils;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class ReadFromFile {
 
     public static List<String> readFromFile(String fileName) throws Exception {
-        return Files.readAllLines(Paths.get(fileName), UTF_8);
+        List<String> lines = new LinkedList<>();
 
+
+        try
+
+        {
+            lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+        } catch (
+                IOException e)
+
+        {
+            System.err.println("wrong file");
+        }
+
+        return lines;
     }
 
 
